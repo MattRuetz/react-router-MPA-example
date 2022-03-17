4fd12f340fafbf3dca14dbe72ae2a7e3aa29ffec
MattRuetz <matt.b.ruetz@gmail.com>
2022-03-17 15:51:28 -0400

    /contact?name=[something] endpt demonstrates reading URL query parameters -- see Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,14 @@
+import { useLocation } from 'react-router-dom';
+
 function Contact() {
+    const queryString = useLocation().search;
+    const queryParams = new URLSearchParams(queryString);
+    const name = queryParams.get('name');
+    //?name=
+
     return (
         <div>
-            <h2>Contact</h2>
+            <h2>Hey {name}, contact me here:</h2>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Suscipit magnam veniam, nam et nisi, iste eveniet perferendis
